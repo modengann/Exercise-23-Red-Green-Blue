59d4780e68b9280762a217da88facd99a72825fd
modengann <99417989+modengann@users.noreply.github.com>
2022-09-12 21:41:40 -0400

    init

---
+++ src/red_green_blue.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import re
+
+def red_green_blue(filename="src/rgb.txt"):
+    
+
+
+
+def main():
+    result = red_green_blue()
+    print()
+
+    #print(f"The result should be a list, it is a {type(result)}
+    #print(f"The length of the list should be 753, was {len(result)}   
+
+    #for s in result:
+    #      if(type(s) != str):
+    #            print("All items in the list should be strings")
+    #   
+       
+    #for i, s in enumerate(result):
+    #    pos = "in result list with index %i: %s" % (i, s)
+    #    t=s.split("\t")
+    #    r=int(t[0])
+    #    g=int(t[1])
+    #    b=int(t[2])
+    #    name=t[3]   
+    #    if(r < 0 or r > 255):
+    #        print("The value of the red component should be in the range [0,255] %s!" % pos)
+    #    if(g < 0 or g > 255):
+    #        print("The value of the green component should be in the range [0,255] %s!" % pos)
+    #    if(b < 0 or b > 255):
+    #        print("The value of the blue component should be in the range [0,255] %s!" % pos)
+
+    #    t = result[1].split("\t")
+    #    r=int(t[0])
+    #    g=int(t[1])
+    #    b=int(t[2])
+    #    name=t[3]
+    #    if r != 248:
+    #        print("Incorrect value of red component in the second string!"
+    #    if g != 248:
+    #        print("Incorrect value of green component in the second string!"
+    #    if b != 255:
+    #        print("Incorrect value of blue component in the second string!"
+    #
+
+
+
+if __name__ == "__main__":
+    main()
